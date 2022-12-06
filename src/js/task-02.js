448c2b50145965e97fe159ec306318ecee9e98e8
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2022-12-06 06:54:47 +0200

    finish hw

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -16,47 +16,25 @@ const ingredients = [
 
 // Заранее прошу прщения, но все перепробовал, в цикле получалось только создать Li, а вот присвоить им класс и textContent никак не дошло как...
 
-const list = document.createElement('ul');
-list.classList.add('#ingredients');
+const list = document.querySelector('#ingredients');
+// list.classList.add('#ingredients');
 // console.log(list);
 
-const li = document.createElement('li');
-li.classList.add('item');
-// console.log(li);
 
-let createList = ingredients.map(ingredient => `<li class = "item">${ingredient}<li>`).join("")
+const newArrey = [];
+for (let i = 0; i < ingredients.length; i++) {
+  let li = document.createElement('li')
+  li.textContent = ingredients[i];
+  li.classList.add('item');
+  newArrey.push(li)
+  
+}
+console.log(newArrey)
 
-// console.log(createList)
-list.insertAdjacentHTML("afterbegin", createList)
-console.log(list)
+list.append(...newArrey)
 
-// const li1 =document.createElement('li');
-// li1.textContent = ingredients[0];
-// li1.classList.add('item')
 
-// const li2 =document.createElement('li');
-// li2.textContent = ingredients[1];
-// li2.classList.add('item')
 
-// const li3 =document.createElement('li');
-// li3.textContent = ingredients[2];
-// li3.classList.add('item')
-
-// const li4 =document.createElement('li');
-// li4.textContent = ingredients[3];
-// li4.classList.add('item')
-
-// const li5 =document.createElement('li');
-// li5.textContent = ingredients[4];
-// li5.classList.add('item')
-
-// const li6 =document.createElement('li');
-// li6.textContent = ingredients[5];
-// li6.classList.add('item')
-
-// list.append(li1,li2,li3,li4,li5,li6)
-
-// console.log(list)
 
 
 
